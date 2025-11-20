d7753a54f4bb6f607cda2ed7591f93c063ff526f
SoTT507 <riccardo.sottoriva@proton.me>
2025-11-20 17:49:28 +0100

    Fix to CMake script due to target_precompile_header n.a. on hpc

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,19 @@
 
 int main() {
     SparseMatrixBenchmark benchmark;
-
     // Add matrices with different sparsity patterns
     benchmark.addMatrixFile("thirdparty/1138_bus/1138_bus.mtx");
     benchmark.addMatrixFile("thirdparty/fl2010/fl2010.mtx");
     //benchmark.addMatrixFile("thirdparty/F1/F1.mtx");
     //benchmark.addMatrixFile("thirdparty/circuit5M/circuit5M.mtx");
-
-    // Or generate random matrices with different sparsity
-    // std::cout << "Generating random matrices with different sparsity...\n";
-
-    // std::vector<double> sparsities = {0.99, 0.95, 0.90, 0.80, 0.50};
-    // for (double sparsity : sparsities) {
-        // COOMatrix coo(5000, 5000);
-        // coo.generateRandomSparse(5000, sparsity);
-        // CSRMatrix csr(coo);
-        //add benchmark for randomly generated sparse matrix
-    // }
+    //
+    //
+    //Considering a piece of code to run warmup directly from program
+    //instead of running the program multiple times as of now
+    //
+    //for(int i = 0; i<=5; i++){
+    //  benchmark.warmup(); //same as runFullBenchmark but does not save nor print results
+    //}
 
     // Run full benchmark
     benchmark.runFullBenchmark();
