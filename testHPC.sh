c6e088fd05b628e0d61b498ae642a6e349e90395
SoTT507 <riccardo.sottoriva@proton.me>
2026-01-05 15:24:16 +0100

    PBS update

---
+++ testHPC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+TOTAL_CORES=$(cat $PBS_NODEFILE | wc -l)
+NODES=$(uniq $PBS_NODEFILE | wc -l)
+
+MPI_PER_NODE_LIST="1 2 4 8"
+
+for MPI_PER_NODE in $MPI_PER_NODE_LIST
+do
+    MPI_PROCS=$((MPI_PER_NODE * NODES))
+    OMP_THREADS=$((TOTAL_CORES / MPI_PROCS))
+
+    export OMP_NUM_THREADS=$OMP_THREADS
+    export OMP_PROC_BIND=close
+    export OMP_PLACES=cores
+
+    echo "MPI=$MPI_PROCS  OMP=$OMP_THREADS"
+
+    mpirun \
+      --map-by ppr:${MPI_PER_NODE}:node \
+      --bind-to core \
+      -np $MPI_PROCS \
+      ./d2_SpMV
+done
